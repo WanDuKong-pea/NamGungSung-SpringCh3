a732d58c42cc7fa43bdfc9870200390ba6100b33
WanDuKong-pea <sophisticateo_o@naver.com>
2023-05-09 22:17:01 +0900

    Spring(DI,AOP)_Spring DI 흉내내기

---
+++ src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -0,0 +1,33 @@
+package com.fastcampus.ch3.diCopy1;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+class Car {}
+class SportsCar extends Car {}
+class Truck extends Car {}
+class Engine {}
+
+/**
+ * 변경에 유리한 코드를 작성해보자
+ * 다형성을 이용.
+ */
+public class Main1 {
+    public static void main(String[] args) throws Exception{
+        Car car = (Car) getObject();
+        //Engine engine = (Engine)getObject();
+        System.out.println("car = " + car);
+    }
+
+    static Object getObject() throws Exception{
+        //config.txt를 읽어서 Properties에 저장
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+
+        //클래스 객체(설계도)를 얻어서
+        Class clazz = Class.forName(p.getProperty("car"));
+        //Class clazz = Class.forName(p.getPropertu("engine"));
+        return clazz.newInstance(); //객체를 생성해서 반환
+    }
+}
